ec4ba98f0468208855b8d14935c9058aa5c5458f
mutabazichristian <c.mutabazi@alustudent.com>
2024-11-29 21:50:00 +0200

    removed some files

--- lib/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color mint = Color(0xFFE0FCFB);
-  static const Color navy = Color(0xFF264654);
-  static const Color teal = Color(0xFF299D8F);
-  static const Color coral = Color(0xFFF3A361);
-  static const Color rust = Color(0xFFE76F51);
-}
